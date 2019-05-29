2ba0ce13cfd8770737e63248d6ad5bd49de870f2
Tom Chen <tom@itri.org.tw>
2019-05-29 21:41:24 +0800

    Add: test import uuid feature

--- handler.ts
+++ handler.ts
@@ -2,10 +2,11 @@ import { APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
 // import * as _ from 'lodash'
 const _ = require('lodash')
+import { v4 as uuid } from 'uuid'
 
 export const helloTsFunc: APIGatewayProxyHandler = async (event, _context) => {
     let lotest: [] = _.range(1911, 2019)
-    let testVersion: string = 'v1.5'
+    let testVersion: string = 'v1.6-' + uuid()
     return {
         statusCode: 200,
         body: JSON.stringify(
