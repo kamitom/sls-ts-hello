522b94bb9cc8da3832145e82d18e2a23b1d8f8d8
Tom Chen <tom@itri.org.tw>
2019-05-31 23:01:45 +0800

    Feature: test typescript import

--- foo.ts
+++ foo.ts
@@ -1,8 +1,8 @@
 const foo1: string = 'foo100'
 const foo2: string = '200foo'
 
-let showmeLengh: string = '99898989a'
+let jpLanguageLength: string = '天皇陛下、亀井静香さんらに大綬章手渡す'
 
 //fix: Module 'xxx' has no default export.
 export default { foo1, foo2 }
-export { showmeLengh }
+export { jpLanguageLength as showJpLanguageLength }
--- handler.ts
+++ handler.ts
@@ -3,8 +3,9 @@ import 'source-map-support/register'
 // import * as _ from 'lodash'
 const _ = require('lodash')
 
-import foo from './foo'
-import { showmeLengh } from './foo'
+// import foo from './foo'
+// import { showmeLengh } from './foo'
+import * as foo2 from './foo'
 
 export const helloTsFunc: APIGatewayProxyHandler = async (event, _context) => {
     let lotest: [] = _.range(1911, 2019)
@@ -17,8 +18,8 @@ export const helloTsFunc: APIGatewayProxyHandler = async (event, _context) => {
                 input: event,
                 theVer: testVersion,
                 lodashTest: lotest.length,
-                theFoo: foo.foo1,
-                theLengthTest: 'String length: ' + showmeLengh.length,
+                theFoo: foo2.default.foo2,
+                fooExport: 'String Contents: ' + foo2.showJpLanguageLength,
             },
             null,
             2
